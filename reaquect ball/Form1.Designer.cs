46e59d34fecd06b75d7cbbbd6a930f7855c39e08
David Dickson <davidick080@ed.amdsb.ca>
2021-01-08 15:08:46 -0500

    Add project files.

---
+++ reaquect ball/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace reaquect_ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tick = new System.Windows.Forms.Timer(this.components);
+            this.Score = new System.Windows.Forms.Label();
+            this.p1Score = new System.Windows.Forms.Label();
+            this.scoreAnouncer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.p2Score = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tick
+            // 
+            this.tick.Enabled = true;
+            this.tick.Interval = 20;
+            this.tick.Tick += new System.EventHandler(this.tick_Tick);
+            // 
+            // Score
+            // 
+            this.Score.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.ForeColor = System.Drawing.SystemColors.Control;
+            this.Score.Location = new System.Drawing.Point(824, 9);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(74, 25);
+            this.Score.TabIndex = 0;
+            this.Score.Text = "score";
+            // 
+            // p1Score
+            // 
+            this.p1Score.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1Score.ForeColor = System.Drawing.SystemColors.Control;
+            this.p1Score.Location = new System.Drawing.Point(822, 32);
+            this.p1Score.Name = "p1Score";
+            this.p1Score.Size = new System.Drawing.Size(33, 32);
+            this.p1Score.TabIndex = 2;
+            this.p1Score.Text = "0";
+            // 
+            // scoreAnouncer
+            // 
+            this.scoreAnouncer.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreAnouncer.ForeColor = System.Drawing.SystemColors.Control;
+            this.scoreAnouncer.Location = new System.Drawing.Point(715, 182);
+            this.scoreAnouncer.Name = "scoreAnouncer";
+            this.scoreAnouncer.Size = new System.Drawing.Size(220, 36);
+            this.scoreAnouncer.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(841, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ":";
+            // 
+            // p2Score
+            // 
+            this.p2Score.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2Score.ForeColor = System.Drawing.SystemColors.Control;
+            this.p2Score.Location = new System.Drawing.Point(852, 32);
+            this.p2Score.Name = "p2Score";
+            this.p2Score.Size = new System.Drawing.Size(27, 27);
+            this.p2Score.TabIndex = 5;
+            this.p2Score.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(937, 493);
+            this.Controls.Add(this.p2Score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scoreAnouncer);
+            this.Controls.Add(this.p1Score);
+            this.Controls.Add(this.Score);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tick;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label p1Score;
+        private System.Windows.Forms.Label scoreAnouncer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label p2Score;
+    }
+}
+
---
+++ reaquect ball/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace reaquect_ball
+{
+    public partial class Form1 : Form
+    {
+        
+        //p1 paddle x&y axis & score
+        int paddle1X = 400;
+        int paddle1Y = 50;
+        int player1Score = 0;
+        
+        //p2 paddle x&y axis & score
+        int paddle2X = 400;
+        int paddle2Y = 390;
+        int player2Score = 0;
+
+        int paddleWidth = 30;
+        int paddleHeight = 30;
+        int paddleSpeed1= 13;
+        int paddleSpeed2 = 13;
+
+        int ballX = 425;
+        int ballY = 256;
+        int ballXSpeed = 5;
+        int ballYSpeed= -5;
+        int ballWidth = 10;
+        int ballHeight = 10;
+        // p1
+        bool dDown = false;
+        bool aDown = false;
+        bool wDown = false;
+        bool sDown = false;
+        // p2 
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool leftDown = false;
+        bool rightDown = false;
+        //for ball reset 
+        int p1Reset;
+        int p2Reset;
+        //sounds 
+        SoundPlayer sound1 = new SoundPlayer(Properties.Resources.neck_snap_Vladimir_719669812);
+        SoundPlayer sound2 = new SoundPlayer(Properties.Resources.Shotgun_SoundBible_com_862990674);
+        //grapics 
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Pen whitePen = new Pen(Color.White);
+        Pen greenPen = new Pen(Color.DarkOliveGreen);
+        public Form1()
+        {
+            InitializeComponent(); 
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+
+        }
+        private void Form1_Keyleft(object sender, KeyEventArgs e)//use for left key 
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+        private void Form1_Keyright(object sender, KeyEventArgs e)//use for left key 
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tick_Tick(object sender, EventArgs e)
+        //To move ball 
+        {
+            ballX += ballXSpeed;
+            ballY += ballYSpeed;
+
+            //move player 1
+            if (wDown == true && paddle1Y > 0)
+            {
+                paddle1Y -= paddleSpeed1;
+            }
+            if (sDown == true && paddle1Y < 225)
+            {
+            paddle1Y += paddleSpeed1;
+            }
+            if (dDown == true && paddle1X < 700- paddleWidth)
+            {
+                paddle1X += paddleSpeed1;
+            }
+            if (aDown == true && paddle1X > 200)
+            {
+                paddle1X -= paddleSpeed1;
+            }
+            //move player 2
+            if (upArrowDown == true && paddle2Y >265)
+            {
+                paddle2Y -= paddleSpeed2;
+            }
+
+            if (downArrowDown == true && paddle2Y < this.Height - paddleHeight-47)
+            {
+                paddle2Y += paddleSpeed2;
+            }
+            if (rightDown == true && paddle2X < 700- paddleWidth)
+            {
+                paddle2X += paddleSpeed2;
+            }
+            if (leftDown == true && paddle2X > 200)
+            {
+                paddle2X -= paddleSpeed2;
+            }
+            //for collision with left & right wall for ball
+            if (ballX > 700||ballX<200)
+            {
+                ballXSpeed *= -1;
+            }
+           // For top & bottom wall
+           //if (ballY > 0 || ballY < this.Height - ballHeight)
+         //  {
+           //    ballYSpeed *= -1;
+          //  }
+            Rectangle player1Rec = new Rectangle(paddle1X, paddle1Y, paddleWidth, paddleHeight);
+            Rectangle player2Rec = new Rectangle(paddle2X, paddle2Y, paddleWidth, paddleHeight);
+            Rectangle ballRec = new Rectangle(ballX, ballY, ballWidth, ballHeight);
+            if (player1Rec.IntersectsWith(ballRec))
+            {
+                sound1.Play();
+                ballYSpeed = 8;
+            //    ballXSpeed=-5;
+                ballY = paddle1Y + paddleHeight+1;
+            }
+               if(player2Rec.IntersectsWith(ballRec))
+            {
+                sound1.Play();
+                ballYSpeed = -8;
+             //   ballXSpeed = -5;
+                ballY = paddle2Y-10;
+            }
+            // when p2 scores on p1 
+           if (ballY < 0)
+            {
+                sound2.Play();
+                player2Score += 1;
+                p2Score.Text = $"{player2Score}";
+                ballX = 425;
+                ballY = 256;
+             //   ballXSpeed = 0;
+              //  ballYSpeed = 0;
+            }
+           if(ballY>this.Height)
+            {
+                sound2.Play();
+                player1Score += 1;
+                p1Score.Text = $"{player1Score}";
+                ballX=425;
+                 ballY=256;
+            //    ballXSpeed = 0;
+            //    ballYSpeed = 0;
+            }
+          
+                //when a player hits 5
+                if (player1Score == 5 || player2Score == 5)
+            {
+                tick.Enabled = false;
+            }
+            
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(whiteBrush, ballX, ballY, ballWidth, ballHeight);
+           
+            e.Graphics.FillEllipse(greenBrush, paddle1X, paddle1Y, paddleWidth, paddleHeight);
+            e.Graphics.FillEllipse(redBrush, paddle2X, paddle2Y, paddleWidth, paddleHeight);
+            e.Graphics.DrawLine(whitePen, 200, 0, 200, 700);
+            e.Graphics.DrawLine(whitePen, 700, 0, 700, 700);
+            e.Graphics.DrawLine(whitePen, 200, 260,700, 260);
+            e.Graphics.DrawEllipse(whitePen,400,225,70,70);
+           // e.Graphics.DrawString($"{player1Score}", screenFont, whiteBrush, 280, 10);
+            //e.Graphics.DrawString($"{player2Score}", screenFont, whiteBrush, 310, 10);
+
+        }
+    }
+}
+
